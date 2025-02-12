a8077e6d2b3a0b0e57aba9e4f4b32696d0c9966f
diego <diegozaratean@gmail.com>
2025-02-12 18:44:02 +0000

    finish movie model

--- src/models.py
+++ src/models.py
@@ -7,6 +7,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
+
+
+
+class Director(Base):
+    __tablename__ = 'director'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    nacionalidad = Column(String(250), nullable=False)
+    numero_peliculas = Column(Integer, nullable=False)
+    
+class Pelicula(Base):
+    __tablename__ = 'pelicula'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String(250), nullable=False)
+    duracion = Column(Integer, nullable=False)
+    genero = Column(String(250), nullable=False)
+    costo_produccion = Column(Integer, nullable=False)
+    director_id = Column(Integer, ForeignKey('director.id'))
+    director = relationship(Director)
+
+
+class Actor(Base):
+    __tablename__ = 'actor'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    nacionalidad = Column(String(250), nullable=False)
+
+
+class PeliculaActor(Base):
+    __tablename__ = 'pelicula_actor'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    pelicula_id = Column(Integer, ForeignKey('pelicula.id'))
+    pelicula = relationship(Pelicula)
+    actor_id = Column(Integer, ForeignKey('actor.id'))
+    actor = relationship(Actor)
+
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
